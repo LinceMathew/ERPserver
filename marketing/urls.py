9f066976f33b608c556c821b1c0d3375e15e5a40
Sreedeep CV <sreedeep200@gmail.com>
2024-12-01 15:15:27 +0530

    added new version to adress api

--- marketing/urls.py
+++ marketing/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('order/<int:pk>/', MarketingRetrieveUpdateDestroyAPIView.as_view()),
     path('item/', ItemListCreateAPIView.as_view()),
     path('item/<int:pk>/', ItemRetrieveUpdateDestroyAPIView.as_view()),
-    path('addresss/', AddressListCreateAPIView.as_view()),
+    path('addresss/v2/', AddressListCreateAPIView.as_view()),
     path('addresss/<int:pk>/', AddressRetrieveUpdateDestroyAPIView.as_view()),
     path('status/<int:pk>/',StatusAPIView.as_view()),
     path('order_suggestions/',order_suggestions)
